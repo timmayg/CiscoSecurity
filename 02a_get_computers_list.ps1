8c8355a3b100290095e76c91aacc2d12cf68f31f
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-17 22:54:22 -0500

    Update 02a_get_computers_list.ps1

--- 02a_get_computers_list.ps1
+++ 02a_get_computers_list.ps1
@@ -3,10 +3,6 @@
 #
 $headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
 $headers.Add('Authorization', 'Basic ZjU3NGI4Y2M5ZDkyMzBhOTY3NzQ6MDg1MDM1MzYtOGE4Zi00ZTI2LWIxN2UtM2YwMmQyOTNkMzFk')
-#$clientid='f574b8cc9d9230a96774'
-#$apikey='08503536-8a8f-4e26-b17e-3f02d293d31d'
-#$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
-#$headers.Add('Authorization', 'Basic '$base64Auth'')
 $headers.Add('accept', 'application/json')
 $headers.Add('Content-type', 'application/json')
 $headers.Add('Accept-Encoding', 'gzip, deflate')

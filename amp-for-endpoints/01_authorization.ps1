d6bbbc78c058231d5b712c7eed3f456781a0f65a
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-17 22:36:15 -0500

    Create 01_authorization.ps1

---
+++ amp-for-endpoints/01_authorization.ps1
@@ -0,0 +1,16 @@
+  
+#
+#  AMP for Endpoints API requires a Client ID and an API Key.
+#  AMP for Endpoints API Authorization supports a Base64 hash of these two keys.
+#  Here we will hash them together using Powershell. 
+#
+#  At this time you can check this website to assure PS is converting the strings correctly.
+#    https://www.base64encode.org/
+#
+
+$clientid='enter-your-client-id'
+$apikey='enter-your-api-key'
+$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
+
+Write-Host $base64Auth
+

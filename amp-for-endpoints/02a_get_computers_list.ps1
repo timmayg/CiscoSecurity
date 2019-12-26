fa06945aa65908b1781c9226652615434f6dcaec
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-25 21:46:47 -0500

    Update 02a_get_computers_list.ps1

--- amp-for-endpoints/02a_get_computers_list.ps1
+++ amp-for-endpoints/02a_get_computers_list.ps1
@@ -1,1 +1,12 @@
 
+$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+$clientid='enter-your-client-id'
+$apikey='enter-your-api-key'
+$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
+$headers.Add('Authorization', 'Basic ' + $base64Auth)
+$headers.Add('accept', 'application/json')
+$headers.Add('Content-type', 'application/json')
+$headers.Add('Accept-Encoding', 'gzip, deflate')
+#
+$url = 'https://api.amp.cisco.com/v1/computers/'
+Invoke-RestMethod -Method Get -URI $url -Headers $headers

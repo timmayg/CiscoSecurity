4f4b88135884fcc6a7a95b941cbaa3c840cce69b
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-26 09:15:25 -0500

    Create 05c_create_new_group.ps1
    https://api-docs.amp.cisco.com/api_actions/details?api_action=POST+%2Fv1%2Fgroups&api_host=api.amp.cisco.com&api_resource=Group&api_version=v1

---
+++ amp-for-endpoints/05c_create_new_group.ps1
@@ -0,0 +1,26 @@
+#
+#Creates a new group, along with a group name or description.
+#
+$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+$clientid = 'enter-your-client-id'
+$apikey = 'enter-your-api-key'
+$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
+$headers.Add('Authorization', 'Basic ' + $base64Auth)
+$headers.Add('accept', 'application/json')
+$headers.Add('Content-type', 'application/json')
+$headers.Add('Accept-Encoding', 'gzip, deflate')
+#
+#
+$data = @{
+    'name' = 'Test Group1'
+    'description' = 'This is for testing'
+}
+#
+#  We need to convert the hashtable to JSON.
+#
+$body = ConvertTo-JSON -InputObject $data
+$url = 'https://api.amp.cisco.com/v1/groups/' 
+
+Invoke-RestMethod -Method Post -URI $url -Headers $headers -Body $body
+
+

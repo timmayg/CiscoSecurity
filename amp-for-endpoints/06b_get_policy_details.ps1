b5c502169ea27219219e66da4a3aea5be9c2aa56
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-26 09:40:22 -0500

    Create 06b_get_policy_details.ps1
    https://api-docs.amp.cisco.com/api_actions/details?api_action=GET+%2Fv1%2Fpolicies%2F%7B%3Apolicy_guid%7D&api_host=api.amp.cisco.com&api_resource=Policy&api_version=v1

---
+++ amp-for-endpoints/06b_get_policy_details.ps1
@@ -0,0 +1,24 @@
+#
+# Using this API, you can retrieve information about a particular policy based on policy_guid.
+#
+$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+$clientid = 'enter-your-client-id'
+$apikey = 'enter-your-api-key'
+$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
+$headers.Add('Authorization', 'Basic ' + $base64Auth)
+$headers.Add('accept', 'application/json')
+$headers.Add('Content-type', 'application/json')
+$headers.Add('Accept-Encoding', 'gzip, deflate')
+#
+#
+#   The policy GUID can be obtained from the API, please see 06a_get_policies_list.ps1. 
+#  
+$policy_guid = '8a9ae814-7441-441b-a4f8-c0f079f2aa85'
+$url = 'https://api.amp.cisco.com/v1/policies/' + $policy_guid
+#Invoke-RestMethod -Method Get -URI $url -Headers $headers
+
+$response = Invoke-RestMethod -Method Get -URI $url -Headers $headers
+Write-Host $response.data
+
+
+

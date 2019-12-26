994f6736e1ad32009773ac01cc650614e1dcd555
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-25 21:59:47 -0500

    Create 02c_get_computer_trajectory.ps1

---
+++ amp-for-endpoints/02c_get_computer_trajectory.ps1
@@ -0,0 +1,16 @@
+#  Shows information about a specific computer.
+#
+$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+$clientid='enter-your-client-id'
+$apikey='enter-your-api-key'
+$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
+$headers.Add('Authorization', 'Basic ' + $base64Auth)
+$headers.Add('accept', 'application/json')
+$headers.Add('Content-type', 'application/json')
+$headers.Add('Accept-Encoding', 'gzip, deflate')
+#
+#
+#   The connector GUID can be obtained in the AMP Console or from the API. 
+#  
+$url = 'https://api.amp.cisco.com/v1/computers/7244b3ea-68bd-4fff-81d8-2f08aef45220/trajectory'
+Invoke-RestMethod -Method Get -URI $url -Headers $headers

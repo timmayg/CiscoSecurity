7af2a1ce6a348013a8fe50d3ff483616696532c1
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-26 09:56:25 -0500

    Create 04_get_events.ps1
    https://api-docs.amp.cisco.com/api_actions/details?api_action=GET+%2Fv1%2Fevents&api_host=api.amp.cisco.com&api_resource=Event&api_version=v1

---
+++ amp-for-endpoints/04_get_events.ps1
@@ -0,0 +1,17 @@
+#
+# This is a general query interface for events. This is analogous to the Events view on the FireAMP Console.
+#
+$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+$clientid = 'enter-your-client-id'
+$apikey = 'enter-your-api-key'
+$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
+$headers.Add('Authorization', 'Basic ' + $base64Auth)
+$headers.Add('accept', 'application/json')
+$headers.Add('Content-type', 'application/json')
+$headers.Add('Accept-Encoding', 'gzip, deflate')
+#
+#
+$url = 'https://api.amp.cisco.com/v1/events' 
+Invoke-RestMethod -Method Get -URI $url -Headers $headers
+
+

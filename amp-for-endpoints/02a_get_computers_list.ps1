abfd97d0fe91d9b730287c84d6d86965fc9968b4
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-26 23:55:34 -0500

    Update 02a_get_computers_list.ps1
    https://api-docs.amp.cisco.com/api_actions/details?api_action=GET+%2Fv1%2Fcomputers&api_host=api.amp.cisco.com&api_resource=Computer&api_version=v1

--- amp-for-endpoints/02a_get_computers_list.ps1
+++ amp-for-endpoints/02a_get_computers_list.ps1
@@ -10,5 +10,6 @@ $headers.Add('accept', 'application/json')
 $headers.Add('Content-type', 'application/json')
 $headers.Add('Accept-Encoding', 'gzip, deflate')
 #
+#
 $url = 'https://api.amp.cisco.com/v1/computers/'
 Invoke-RestMethod -Method Get -URI $url -Headers $headers

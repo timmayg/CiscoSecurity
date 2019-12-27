3e36d63dfcb303ef74c5eac10b925377742400d6
Tim Glen <16306246+timmayg@users.noreply.github.com>
2019-12-26 22:00:39 -0500

    Create 07a_get_simple_custom_dection_lists.ps1
    https://api-docs.amp.cisco.com/api_actions/details?api_action=GET+%2Fv1%2Ffile_lists%2Fsimple_custom_detections&api_host=api.amp.cisco.com&api_resource=File+List&api_version=v1

---
+++ amp-for-endpoints/07a_get_simple_custom_dection_lists.ps1
@@ -0,0 +1,18 @@
+#
+#  Returns a list of simple custom detection file lists. You can filter this list by name.
+#
+$headers = New-Object 'System.Collections.Generic.Dictionary[[String],[String]]'
+$clientid = 'enter-your-client-id'
+$apikey = 'enter-your-api-key'
+$base64Auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $clientid,$apikey))) 
+$headers.Add('Authorization', 'Basic ' + $base64Auth)
+$headers.Add('accept', 'application/json')
+$headers.Add('Content-type', 'application/json')
+$headers.Add('Accept-Encoding', 'gzip, deflate')
+#
+#
+$url = 'https://api.amp.cisco.com/v1/file_lists/simple_custom_detections'
+Invoke-RestMethod -Method Patch -URI $url -Headers $headers
+
+
+
